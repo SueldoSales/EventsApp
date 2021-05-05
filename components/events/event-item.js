6b382c28603ce47ee23c1c64a236cdd42eddc2de
Sueldo Sales <sueldosales@gmail.com>
2021-05-05 14:38:25 -0300

    [UPD] Event Detail

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -16,7 +16,7 @@ export default function EventItem(props) {
 
     const formattedAddress = location.replace(', ', '\n');
 
-    const exploreLink = `/event/${id}`;
+    const exploreLink = `/events/${id}`;
 
     return (
         <li className={styles.item}>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,33 @@
+import { useRouter } from 'next/router';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventContent from '../../components/event-detail/event-content';
+
+import { getEventById } from '../../dummy-data';
+
 export default function EventDetailPage() {
-    return (
-      <h1>Event Detail</h1>
-    )
+
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics 
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  )
 }
